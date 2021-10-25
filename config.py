a26ef7c054cba9f03dcdf834139c3630d7013bca
roxyconfig <92932568+roxyconfig@users.noreply.github.com>
2021-10-25 18:23:06 +0400

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    BOT_TOKEN = "2068263233:AAHOkkKbxUqvjSBvvHqw4U9IadAjI5ykQLA" # 🌚 get it from @botfather (telegram) by sending /newbot command.
+    BOT_TOKEN = "2068263233:AAGLa99ExDiE-F18d4hD4czQ7jnIGU5ybcI" # 🌚 get it from @botfather (telegram) by sending /newbot command.
     API_ID = 4967403 # 🌚 Same as APP_ID. Get it from my.telegram.org.
     API_HASH = "3290586984b814b9f10d31a83780d1aa" # 🌚 Get it from my.telegram.org.
